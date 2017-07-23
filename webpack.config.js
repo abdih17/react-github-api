a8938600582357d6940b1e65bf067305cf711a7b
Hawa Abdi <abdih17@hotmail.com>
2017-07-23 00:22:47 -0700

    initial scaffold of necessary files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: '${__dirname}/src/main.js',
+  output: {
+    filename: 'bundle-[hash].js',
+    path: '${__dirname}/build',
+    publicPath: '/'
+  },
+  plugin: {
+    new HtmlPlugin('${__dirname}/src/index.html'),
+    new ExtractPlugin('bundle-[hash].css')
+  },
+  module: {
+    rules: {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    rules: {
+      test: /\.scss$/,
+      loader: ExtractPlugin.extract(['sass-loader', 'css-loader'])
+    }
+  }
+}
